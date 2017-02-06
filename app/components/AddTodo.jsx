76de291dd3c90c3af8d794d2f8875baa53bc3e02
grubnubble <montanalisagoodman@gmail.com>
2017-02-06 13:43:02 -0500

    Create AddTodo component

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+
+var AddTodo = React.createClass({
+	handleSubmit: function (e) {
+		e.preventDefault();
+		var strTodo = this.refs.todoText.value;
+
+		if (strTodo.length >0) {
+			this.refs.todoText.value = '';
+			this.props.onAddTodo(strTodo);
+		} else {
+			this.refs.todoText.focus();
+			this.props.onAddTodo("No text entered");
+		}
+	},
+	render: function () {
+
+		return (
+		        <div>
+		        	<form ref="form" onSubmit={this.handleSubmit} className="addtodo-form">
+		        		<input type="text" ref="todoText" placeholder="What do you need to do?"/>
+		        		<button className="button expanded">Add Task</button>
+		        	</form>
+		        </div>
+		)
+	}
+})
+
+module.exports = AddTodo;
---
+++ app/tests/components/AddTodo.test.jsx
@@ -0,0 +1,36 @@
+//testing dependencies
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var AddTodo = require('AddTodo');
+
+describe('AddTodo', () => {
+	it('should exist', () => {
+		expect('AddTodo').toExist();
+	});
+
+	it('should call onAddTodo if any non-empty string entered', () => {
+		var spy = expect.createSpy();
+		var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+		var $el = $(ReactDOM.findDOMNode(addTodo));
+
+		addTodo.refs.todoText.value = 'Hello there';
+		TestUtils.Simulate.submit($el.find('form')[0]);
+
+		expect(spy).toHaveBeenCalledWith('Hello there');
+	});
+
+	it('should alert with "No text entered" if empty string entered', () => {
+		var spy = expect.createSpy();
+		var addTodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy}/>);
+		var $el = $(ReactDOM.findDOMNode(addTodo));
+
+		addTodo.refs.todoText.value = '';
+		TestUtils.Simulate.submit($el.find('form')[0]);
+
+		expect(spy).toHaveBeenCalledWith('No text entered');
+	});
+});
