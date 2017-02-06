f7b7826e0bbfcb4ffdce9862929939cd74e2aef2
grubnubble <montanalisagoodman@gmail.com>
2017-02-06 16:47:55 -0500

    Complete handleAddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var uuid = require('node-uuid');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var Search = require('Search');
@@ -10,23 +11,31 @@ var TodoApp = React.createClass({
 			searchText: '',
 			todos: [
 				{
-					id: 1,
+					id: uuid(),
 					text: 'Walk the dog'
 				}, {
-					id: 2,
+					id: uuid(),
 					text: 'Clean the kitchen'
 				}, {
-					id: 3,
+					id: uuid(),
 					text: 'Craft night with friends'
 				}, {
-					id: 4,
+					id: uuid(),
 					text: 'Appropriate the means of production'
 				}
 			]
 		};
 	},
 	handleAddTodo: function (text) {
-		alert('new todo:' + text);
+		this.setState({
+			todos: [
+				...this.state.todos,
+				{
+					id: uuid(),
+					text: text
+				}
+			]
+		});
 	},
 	handleSearch: function (showCompleted, searchText) {
 		this.setState({
