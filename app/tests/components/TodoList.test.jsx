5afee0cf0eea1d17bf05e4c08d6eb1528c7cdc66
grubnubble <montanalisagoodman@gmail.com>
2017-02-06 10:02:59 -0500

    Add test suite for TodoList

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,30 @@
+//testing dependencies
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var TodoList = require('TodoList');
+var Todo = require('Todo');
+
+describe('TodoList', () => {
+	it('should exist', () => {
+		expect('TodoList').toExist();
+	});
+
+	it('should render one Todo component for each task on the list', () => {
+		var todos= [{
+			id: 1,
+			text: 'Do a thing'
+		}, {
+			id: 2,
+			text: 'Check some stuff'
+		}];
+		var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+		var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+		expect(todosComponents.length).toBe(todos.length);
+	});
+});
+
